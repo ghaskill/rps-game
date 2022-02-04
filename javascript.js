8e12534425ebf7583cbc40d7d78d898b914618fb
ghaskill <ghaskill@gmail.com>
2022-02-04 01:55:18 -0500

    Finished writing main game loop

--- javascript.js
+++ javascript.js
@@ -4,23 +4,46 @@ let rpsArray = [
   "Scissors"
 ];
 
-let playerInput = prompt("Rock, paper, or scissors?");
+//let playerInput = prompt("Rock, paper, or scissors?");
 
-const playerChoice = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+//const playerChoice = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
 
-let computerPlay = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+//let computerPlay = rpsArray[Math.floor(Math.random() * rpsArray.length)];
 
-const computerChoice = computerPlay;
+//const computerChoice = computerPlay;
 
 function rockPaperScissors(playerChoice, computerChoice) {
-  console.log('Computer chose ' + computerChoice + '!');
   if (playerChoice === computerChoice) {
-    console.log("It's a tie!")
+    console.log("tie");
   } else if (playerChoice.length - computerChoice.length == -4 ||
              playerChoice.length - computerChoice.length == 3 ||
              playerChoice.length - computerChoice.length == 1) {
-             console.log("Winrar!");
+      console.log("win");
+      return userWins++;
   } else {
-    console.log("Big sad hours");
+    console.log("loss");
+    return computerWins++;
+  }
+}
+
+function gameLoop() {
+
+  i = 0;
+  userWins = 0;
+  computerWins = 0;
+
+  while (i < 5) {
+    let playerInput = prompt("Rock, paper, or scissors?");
+    let playerChoice = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+    let computerPlay = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    let computerChoice = computerPlay;
+
+    rockPaperScissors(playerChoice, computerChoice);
+    i++;
+  }
+  if (userWins > computerWins) {
+    console.log("Player wins " + userWins + " to " + computerWins);
+  } else if (computerWins > userWins) {
+    console.log("Witness the rise of the machine");
   }
 }
