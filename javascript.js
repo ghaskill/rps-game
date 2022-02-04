b05db85942643fa76b9b2b16cb8b936bb905a134
ghaskill <ghaskill@gmail.com>
2022-02-04 00:32:24 -0500

    Wrote function for individual rounds

--- javascript.js
+++ javascript.js
@@ -4,6 +4,23 @@ let rpsArray = [
   "Scissors"
 ];
 
+let playerInput = prompt("Rock, paper, or scissors?");
+
+const playerChoice = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+
 let computerPlay = rpsArray[Math.floor(Math.random() * rpsArray.length)];
 
-console.log(computerPlay);
+const computerChoice = computerPlay;
+
+function rockPaperScissors(playerChoice, computerChoice) {
+  console.log('Computer chose ' + computerChoice + '!');
+  if (playerChoice === computerChoice) {
+    console.log("It's a tie!")
+  } else if (playerChoice.length - computerChoice.length == -4 ||
+             playerChoice.length - computerChoice.length == 3 ||
+             playerChoice.length - computerChoice.length == 1) {
+             console.log("Winrar!");
+  } else {
+    console.log("Big sad hours");
+  }
+}
